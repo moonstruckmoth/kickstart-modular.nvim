011b13c3a0b347086d5da146d683da5d18b7b8d5
Raj N <moonstruckmoth@gmail.com>
2024-08-04 19:42:15 +0530

    configuring indent-line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,30 @@
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
+    opts = function()
+      return {
+        indent = {
+          char = '│',
+          tab_char = '│',
+        },
+        scope = { show_start = false, show_end = false },
+        exclude = {
+          filetypes = {
+            'help',
+            'alpha',
+            'dashboard',
+            'neo-tree',
+            'Trouble',
+            'trouble',
+            'lazy',
+            'mason',
+            'notify',
+            'toggleterm',
+            'lazyterm',
+          },
+        },
+      }
+    end,
     main = 'ibl',
-    opts = {},
   },
 }
